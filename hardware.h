45fb2737f1cb9511bd69ddcb2e521b02908c30cd
Ahmad <ahmadalberry06@gmail.com>
2025-10-09 19:15:01 +0100

    initialised cpu and definitions

---
+++ hardware.h
@@ -0,0 +1,30 @@
+typedef unsigned char BYTE;
+typedef char SIGNED_BYTE;
+typedef unsigned short WORD;
+typedef short SIGNED_WORD;
+
+#define FLAG_Z 0b01000000
+#define FLAG_N 0b00100000
+#define FLAG_H 0b00010000
+#define FLAG_C 0b00001000
+
+#define BLACK 0b000000001
+#define DARK_GREY 0b00000010
+#define LIGHT_GREY 0b00000100
+#define WHITE 0b00001000
+
+union Register{
+    WORD full;
+    struct 
+    {
+        BYTE lo;
+        BYTE hi;
+    };
+};
+
+
+
+
+
+
+
